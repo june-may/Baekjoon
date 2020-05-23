47cbc5b044b7f9c172a21dba6569023d25fa8abd
june-may <21900052@handong.edu>
2020-05-23 22:09:56 +0900

    5/23 update - 6

---
+++ C/lev_0/2442.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+	
+	int num ;
+
+	scanf("%d", &num);
+	if(num < 1|| num > 100) return 0;
+
+	for(int i = 0 ; i < num ; i++){
+		for(int j = i ; j < num-1 ; j++)
+			printf(" ");
+		for(int k = 0 ; k < (i*2) + 1 ; k++)
+			printf("*");
+		printf("\n");
+	}
+}
