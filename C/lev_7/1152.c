42b726bfb3a405aef91a99117f1ceb7fbfde559a
june-may <21900052@handong.edu>
2020-05-25 13:49:31 +0900

    5/25 update - 2

--- C/lev_7/1152.c
+++ C/lev_7/1152.c
@@ -5,14 +5,18 @@ int main(){
 
 	char str[1000000];
 	int num = 0;
+	int check = 1;
 
 	fgets(str, 1000000, stdin);
 
 	for(int i = 0 ; i < strlen(str); i++){
+			
 		if(str[i] == ' ')
-			num++;
+			check = 1;
+		else if(check){
+			num ++;
+			check = 0;
+		}	
 	}
-
-	printf("%d\n",num+1);
-
+	printf("%d\n", num);
 }
