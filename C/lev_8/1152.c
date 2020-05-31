5e532ac7bac4adbf8e6631e7dc490b547194d90c
june-may <21900052@handong.edu>
2020-05-31 23:29:02 +0900

    5/31 update

--- C/lev_8/1152.c
+++ C/lev_8/1152.c
@@ -4,12 +4,12 @@
 int main(){
 
 	char str[1000000];
-	int num = 0 ;
+	int number = 0 ;
 
 	gets_s(str, 1000000);
 
 	for(int i = 0 ; str[i] != '\0'; i++){
 		if(str[i] == ' ')
-			num++;
+			number++;
 	}
 }
