51dbc58e75a0ede0c83c518ba27e96dea7d8b817
june-may <21900052@handong.edu>
2020-06-03 17:20:19 +0900

    up

--- C/lev_0/2751_quick.c
+++ C/lev_0/2751_quick.c
@@ -1,5 +1,25 @@
 #include <stdio.h>
 
 int main(){
+	
+	int num;
+	int arr[1000000];
 
+	scanf("%d", &num);
+
+	for(int i = 0 ; i < num ; i ++)
+		scanf("%d", &arr[i]);
+
+	for(int i = 0 ; i < num-1 ; i++){
+		for(int j = 0 ; j < num; j++){
+			if(arr[j] > arr[j+1]){
+				int tmp = arr[j];
+				arr[j] = arr[j+1];
+				arr[j+1] = tmp;
+			}
+		}
+	}
+
+	for(int i = 0 ; i < num ; i++)
+		printf("%d\n", arr[i]);
 }
