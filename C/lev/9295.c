7a456b5240ca18454e553168500acefb374f27f5
june-may <21900052@handong.edu>
2020-06-06 12:24:35 +0900

    6/7 update - 2

--- C/lev/9295.c
+++ C/lev/9295.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
 
 int main(){
+	
+	int num;
+	int sum[100];
 
+	scanf("%d", &num);
+
+	for(int i = 0 ; i < num ; i++){
+		int a, b;
+
+		scanf("%d %d", &a, &b);
+		sum[i] = a+b;
+	}
+
+	for(int i = 0 ; i < num ; i++)
+		printf("Case %d: %d\n", i+1,sum[i]);
 }
