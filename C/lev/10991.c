93edcc2c6056a6b2fb6fa4ffad04d5eefdc77b45
june-may <21900052@handong.edu>
2020-06-08 16:28:09 +0900

    6/8 update - 1

---
+++ C/lev/10991.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+
+
+	int num;
+	scanf("%d", &num);
+
+	for(int i = 0 ; i < num ; i++){
+		for(int j = i ; j < num-1 ; j++)
+			printf(" ");
+		for(int k = 0 ; k <= i ; k++)
+			printf("* ");
+		printf("\n");
+	}
+}
