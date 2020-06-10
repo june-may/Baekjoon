796a0fe873835557d77c1568fd95683643eb66ad
june-may <21900052@handong.edu>
2020-06-11 08:12:36 +0900

    6/11 update - 4

--- C/lev/15963.c
+++ C/lev/15963.c
@@ -6,12 +6,11 @@ int main(){
 	int my;
 	int you;
 	scanf("%d %d", &my, &you);
-	if(my > 10 || you > 10) return 0;
 
 	if(my == you)
 		printf("1\n");
 	else
-		printf("0\n";
+		printf("0\n");
 
 	return 0;
 }
