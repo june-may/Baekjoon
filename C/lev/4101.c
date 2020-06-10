1138c22ae686b63a049a39830fd625a4644417c8
june-may <21900052@handong.edu>
2020-06-10 21:41:52 +0900

    6/10 update - 3

---
+++ C/lev/4101.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+	
+	while(1){
+		int fir, sec;
+		scanf("%d %d", &fir, &sec);
+
+		if(fir == 0 && sec == 0) return 0;
+
+		if(fir > sec)
+			printf("Yes\n");
+		else
+			printf("No\n");
+	}
+
+}
