3c3a4209ebf9c5688ea026b1f61a8530de360b49
june-may <21900052@handong.edu>
2020-06-12 10:34:54 +0900

    6/12 update - 2

--- C/lev/10170.c
+++ C/lev/10170.c
@@ -3,6 +3,18 @@
 int main(){
 
 	printf("\n");
+	printf("\n");
+	printf("\n");
+	printf("\n");
+	printf("\n");
+	printf("\n");
+	printf("\n");
+	printf("\n");
+	printf("\n");
+	printf("\n");
+	printf("\n");
+	printf("\n");
+	printf("\n");
 
 	return 0;
 }
