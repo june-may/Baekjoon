d83c6ac398aab1783c40e361fc297875652b855a
june-may <21900052@handong.edu>
2020-06-12 11:05:15 +0900

    6/12 update - 4

--- C/lev_4/8958.c
+++ C/lev_4/8958.c
@@ -1,30 +1,27 @@
 #include <stdio.h>
+#include <string.h>
 #define MAX 80
 
 int main(){
 	
-	int i, j, num, score[MAX] = {0};
+	int i, j, num, score, sum = {0};
 	char OX[80];
 
 	scanf("%d ", &num);
 
 	for(i = 0 ; i < num ; i++){
-		int plus = 0;
+		score = 1;
+		sum = 0;
+		scanf("%s", OX);
 
-		for(j = 0 ; j < MAX ; j++){
-			scanf("%c", &OX[j]);
+		for(j = 0 ; j < strlen(OX); j++){
 			
 			if(OX[j] == 'O')
-				score[i] += ++plus;
-			else
-				plus = 0;
-			
-			if(OX[j]=='\0')
-				break;
+				sum += score++;
+			if(OX[j] == 'X')
+				score = 1;
 		}		
-		
+		printf("%d\n", sum);		
 	}
 	
-	for(i = 0 ; i < num ; i++)
-		printf("%d\n", score[i]);
 }
