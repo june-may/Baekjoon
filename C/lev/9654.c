433ccc353d0901b658f5009a3da26f44cfccffa1
june-may <21900052@handong.edu>
2020-06-14 09:20:43 +0900

    6/14 update - 5

--- C/lev/9654.c
+++ C/lev/9654.c
@@ -2,11 +2,10 @@
 
 int main(){
 
-	printf("SHIP NAME\t\tCLASS\t\tDEPLOYMENT IN SERVICE\n");
-	printf("N2 Bomber\t\tHeavy Fighter\tLimited\t\t21\n");
-	printf("J-Type 327\t\tLight Combat\tUnlimited\t1\n");
-	printf("NX Cruiser\t\tMedium Fighter Limited\t\t18\n");
-	printf("N1 Starfighter\t\tMedium Fighter Unlimited\t25\n");
-	printf("Royal Cruiser\t\tLight Combat\tLimited\t\t4\n");
-
+	printf("SHIP NAME      CLASS          DEPLOYMENT IN SERVICE\n");
+	printf("N2 Bomber      Heavy Fighter  Limited    21        \n");
+	printf("J-Type 327     Light Combat   Unlimited  1         \n");
+	printf("NX Cruiser     Medium Fighter Limited    18        \n");
+	printf("N1 Starfighter Medium Fighter Unlimited  25        \n");
+	printf("Royal Cruiser  Light Combat   Limited    4         \n");
 }
